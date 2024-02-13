fc33b1ebf842050e28eac9e8f4149181710c25bb
Yriy Fedchenko <y.fedchenko@gmail.com>
2024-02-13 22:20:41 +0200

    add react routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { lazy } from 'react';
+import Layout from './Layout';
+
+const Home = lazy(() => import('./pages/HomePage/HomePage'));
+const Catalog = lazy(() => import('./pages/CatalogPage/CatalogPage'));
+const Favorites = lazy(() => import('./pages/FavoritesPage/FavoritesPage'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/favorites" index element={<Favorites />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
   );
 }
 
