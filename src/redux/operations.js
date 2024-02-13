eb5094f1c9c67d5d8aa2f111debc6433a56a73c4
Yriy Fedchenko <y.fedchenko@gmail.com>
2024-02-13 22:23:17 +0200

    add Redux logic

---
+++ src/redux/operations.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://65b39409770d43aba47a096e.mockapi.io/';
+
+export const fetchPaginatedCars = createAsyncThunk('adverts', async (page, { rejectWithValue }) => {
+  try {
+    const response = await axios.get(`adverts?page=${page}&limit=10`);
+    if (response.data.length === 0) {
+      console.error('No more cars avaible');
+    }
+    return response.data;
+  } catch (error) {
+    return rejectWithValue(error.response.data);
+  }
+});
+
+export const fetchAllCars = createAsyncThunk('allAdverts', async (page, { rejectWithValue }) => {
+  try {
+    const response = await axios.get(`adverts`);
+    return response.data;
+  } catch (error) {
+    return rejectWithValue(error.response.data);
+  }
+});
---
+++ src/redux/selectors.js
@@ -0,0 +1,6 @@
+export const selectAdverts = state => state.cars.adverts;
+export const selectAllAdverts = state => state.cars.allAdverts;
+export const selectFavorites = state => state.cars.favorites;
+export const selectIsMount = state => state.cars.isMount;
+export const selectIsLoading = state => state.cars.isLoading;
+export const selectError = state => state.cars.error;
